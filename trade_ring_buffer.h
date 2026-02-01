ada3e7c62693bcb81c1385b25d8fe9583022eddc
Raunil-Singh <raunilsingh.231cs148@nitk.edu.in>
2026-02-02 04:07:30 +0530

    Fixed the class

--- trade_ring_buffer.h
+++ trade_ring_buffer.h
@@ -8,29 +8,33 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <atomic>
 
 namespace TradeRingBuffer {
 
     constexpr std::string_view filename {"/Trade_Ring_Buffer"};
 
-    const uint64_t RING_SIZE {1024LL*1024LL};   // number of blocks of data
-    const uint64_t MOD {739474490608277033LL};  // A randomly generated 18 digit prime number for sequence number wrap-around
+    constexpr uint64_t RING_SIZE {1 << 20};     // number of blocks of data
+    constexpr uint64_t MOD       {1LL << 60};   // used for sequence number wrapping 
 
     // Struct that stores the 
     struct alignas(64) item_node {
         Trade curr_trade;
-        _Atomic uint64_t seq;
+        std::atomic<uint64_t> seq;
     };
 
     struct ring_buffer {
-        item_node ring[RING_SIZE];              // Stores the ring
-        _Atomic pid_t producer_pid;             // Stores the pid of the producer that can used to check if producer is alive
+        item_node ring[RING_SIZE];                      // Stores the ring
+        alignas(64) std::atomic<pid_t> producer_pid;    // Stores the pid of the producer that can used to check if producer is alive
     };
 
-
+    // Class that implements the ring buffer functionality
+    // All methods are non-blocking and use atomic operations for synchronization
+    // 
     class trade_ring_buffer {
     public:
         explicit trade_ring_buffer(bool);
+        ~trade_ring_buffer();
 
         // Producer APIs
         bool add_trade(Trade &);                // Function used for adding data to ring buffer
@@ -42,6 +46,9 @@ namespace TradeRingBuffer {
         Trade get_trade();                      // Returns an copy of the data
 
     private:
+        void update_index_and_seq();            // Updates index and next_expected_seq after reading a trade
+
+        ring_buffer * rb;                       // Pointer to the shared ring buffer
         uint64_t index;                         // stores the index in the ring buffer that is being accessed
         uint64_t next_expected_seq;             // calculated using MOD
         bool is_producer;                       // true if the instance is a producer   
@@ -49,3 +56,19 @@ namespace TradeRingBuffer {
 };
 
 #endif
+
+
+
+
+/*
+
+Some things to note:
+- MOD and RING_SIZE constants are powers of 2 to allow for faster modulo operations using bitwise AND
+- All the APIs run deterministically with minimum logic branches to reduce latency
+- All the data is cache aligned to reduce false sharing and improve performance
+
+TODO:
+- Try getting rid of MOD entirely by experimentally verifying that it is not needed with 64-bit seq numbers
+- Extend the producer identity because pid can be reused by the OS after a process exits
+
+*/
