15ad9a3088baa601cdcd6006b51b73755a91f611
Shashank <shashankshreyanp@gmail.com>
2026-03-28 19:17:14 +0530

    Pulled trade_ring_buffer and order.h from main

--- order.h
+++ order.h
@@ -19,18 +19,19 @@ enum class OrderType {
 enum class OrderExecutionType{
     MARKET,             
     LIMIT,
-    STOP_LOSS,
-    ICEBERG
+    //FOK,
+    //IOC,
+    //ICEBERG
 };
 
 //Order Structure
 typedef struct {
     uint64_t order_id;
     uint64_t price;
-    uint64_t symbol_id;
     uint64_t timestamp;
     uint64_t trader_id;
     OrderType type;  
+    uint32_t symbol_id;                 // lower  bits for symbol id, upper 22 bits 
     uint32_t quantity;
     OrderExecutionType execution_type;
 } Order;
